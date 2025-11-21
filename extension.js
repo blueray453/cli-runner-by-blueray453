36ccf806b8b263c21c1ff90f717367147da14b69
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-22 02:15:52 +0600

    Alhamdulillah, few changes

--- extension.js
+++ extension.js
@@ -1,34 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cli-runner-by-blueray453" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('extension.cli-runner-by-blueray453', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from cli-runner-by-blueray453!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	setTimeout(() => {
+		vscode.commands.executeCommand('extension.cli-runner-by-blueray453');
+	}, 3000);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	console.log('CLI Runner extension deactivated.');
+}
 
 module.exports = {
 	activate,
